45c713bf1d7d5af1d4f16969829771ca84629cea
Serhii Berdianskyi <zeferot94@gmail.com>
2023-10-22 16:37:55 +0300

    Wrote tests for Check password function

--- app/test_main.py
+++ app/test_main.py
@@ -1,1 +1,20 @@
-# write your code here
+import pytest
+from app.main import check_password
+
+
+@pytest.mark.parametrize(
+    "password, expected",
+    [
+        ("Pass@wo1", True),
+        ("Pass@wo1Pass@wo1", True),
+        ("Pass@word1Pass@word1", False),
+        ("Pa@1", False),
+        ("Pass@word", False),
+        ("Pass@word", False),
+        ("Password1", False),
+        ("@assword1", False),
+        ("@asswoвd1", False),
+    ]
+)
+def test_check_password(password: str, expected: bool) -> None:
+    assert check_password(password) == expected
